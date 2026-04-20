d0c54179b32cafa2a8c20d88c614b20d2d215525
kose2888 <ethan.kostiuk@gmail.com>
2026-04-20 12:26:33 -0600

    Added admin models and initial car data

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,17 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
+class CarModelInline(admin.StackedInline):
+    model = CarModel
+    extra = 2
 
-# Register your models here.
+class CarModelAdmin(admin.ModelAdmin):
+    list_display = ['car_make', 'name', 'type', 'year']
+    search_fields = ['car_make', 'name', 'type', 'year']
 
-# CarModelInline class
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+    list_display = ['name', 'description']
 
-# CarModelAdmin class
-
-# CarMakeAdmin class with CarModelInline
-
-# Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
